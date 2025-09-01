639186e3ae7970b8b696c078252ba13021414c6e
jasminvansh11 <jasminvansh06@gmail.com>
2025-09-01 10:33:49 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,32 @@
+"use strict" // treat all JS code as newer version
+
+//alert(3+3) // we are using node js, not a browser
+
+console.log(3+
+    3
+) // code readablity should be high
+
+
+console.log("Jasmin");
+
+let name = "jasmin"
+let age = 19;
+let isLogged = true;
+let state; // undefined value
+
+// number => 2 to power 53
+// big int 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => unique 
+
+
+// object 
+
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) // object
+
+
