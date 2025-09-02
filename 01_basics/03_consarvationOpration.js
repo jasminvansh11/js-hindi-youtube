9b61f60b5396f8c98ccda1052f693f30acddb107
jasminvansh11 <jasminvansh06@gmail.com>
2025-09-02 09:24:10 +0000

    convarsiton of datatypes

---
+++ 01_basics/03_consarvationOpration.js
@@ -0,0 +1,34 @@
+// let score = "335554ff" // not a number
+// let score = "3355";   // number 
+// let score = null;    // number 0
+// let score;          //not a number 
+
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//"33" => 33
+//"33tt" => NaN(Not a Number)
+// true => 1; false =>0
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIN = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIN);
+
+// 1 => true
+// 2 => false
+// "" => false
+// "jasmin" => true
+
+
+let someNum = 33
+let stringNumber = String(someNum);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
