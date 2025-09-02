8fac60e83245812d049130d0909e29d780a81d62
jasminvansh11 <jasminvansh06@gmail.com>
2025-09-02 10:46:14 +0000

    datatype-summary

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,40 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100 //number
+const scoreValue = 100.3 //number
+
+const isLoggedIn = false // boolean
+const outsideTemp = null // object
+let userEmail;
+
+const id = Symbol('123') // symbol
+const anotherId = Symbol('123') // symbol
+
+console.log(id === anotherId); //output:false
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]; //array
+
+
+let myObj = {
+    name: "hitesh",
+    age: 22,
+};
+
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId); //symbol
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
