0734317e862383eeff406647708a47af0458a33e
jasminvansh11 <jasminvansh06@gmail.com>
2025-09-06 10:59:15 +0000

    Maths use in js

---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,43 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+console.log(balance.toString().length);
+// console.log(balance.toFixed(2))
+
+const othereNumber = 23.78945
+// console.log(othereNumber.toPrecision(2))
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+/*              Maths            */
+
+console.log(Math);
+console.log(Math.abs(-4))
+console.log(Math.round(4.9)); // round of value
+console.log(Math.ceil(4.8)); //  take high value
+console.log(Math.floor(4.8)); // take lowest value
+console.log(Math.min(1,2,5,4,7,89,9)); // Give minimum value
+console.log(Math.max(1,89,7,8,88,9,99)); // Give maximum value
+
+console.log(Math.random())
+console.log((Math.random()*10) + 1)
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * ((max - min)) + min));
+
+
+
+
+
+
+
+
+
+
