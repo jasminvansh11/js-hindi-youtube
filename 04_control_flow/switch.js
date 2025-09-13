7faac22c86ed5a1198bc56b3b999435c205d379b
jasminvansh11 <jasminvansh06@gmail.com>
2025-09-13 06:57:38 +0000

    control flow code files

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,56 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = 13
+
+switch (month){
+    case 1: console.log("Jan")
+    break;
+
+    case 2: console.log("Feb")
+    console.log("Feb")
+    break;
+
+    case 3: console.log("March")
+    break;
+
+    case 4: console.log("April")
+    break;
+
+    case 5: console.log("May")
+    break;
+
+    case 6: console.log("June")
+    break;
+
+    case 7: console.log("July")
+    break;
+
+    case 8: console.log("Aug")
+    break;
+
+    case 9: console.log("Sept")
+    break;
+
+    case 10: console.log("Oct")
+    break;
+
+    case 11: console.log("Nov")
+    break;
+
+    case 12: console.log("Dec")
+    break
+    
+
+    default : if( month <= 13){
+        console.log("Invald input")
+    }
+}
+
